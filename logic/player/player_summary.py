08a7de48de5b2c360df45fdb1fe8b5c31e464fd2
LiXiaoqiang <lixiaoqiang@LiXiaoqiangsAir.attlocal.net>
2016-02-01 21:30:45 -0800

    haha

---
+++ logic/player/player_summary.py
@@ -0,0 +1,13 @@
+from api.cassiopeia.cassiopeia import riotapi
+
+def main():
+    # Setup riotapi
+    riotapi.set_region("NA")
+    riotapi.print_calls(True)
+    key = 'b7190b84-484d-4cc7-88ca-8e2b90fb7f56'  # You can create an env var called "DEV_KEY" that holds your developer key. It will be loaded here.
+    riotapi.set_api_key(key)
+    print "this is life"
+
+
+if __name__ == "__main__":
+    main()
